e291863a4e269610b63d9c23ed4d4d881949f0e9
bryancitu <bryanjesus.ra@gmail.com>
2020-11-24 15:35:44 -0500

    creando nuestro modelo de los usuarios personalizado

--- cajaperusa/settings.py
+++ cajaperusa/settings.py
@@ -107,6 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = 'usuarios.Usuarios'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager 
 
 # Create your models here.
+
+class Usuarios(AbstractBaseUser, PermissionsMixin):
+
+    username    = models.CharField(max_length=30, unique=True)
+    email       = models.EmailField(max_length=254, unique=True)
+    first_name  = models.CharField(max_length=50)
+    last_name   = models.CharField(max_length=50)
+
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    REQUIRED_FIELDS = ['email',]
+
+    objects = UserManager()
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.apellidos

8f652f54b925328d963fef13a5c41cd50ace361c
bryancitu <bryanjesus.ra@gmail.com>
2020-11-24 18:59:55 -0500

    registrarse completado

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,7 +3,40 @@ from .models import Usuarios
 
 class UsuarioRegisterForm(forms.ModelForm):
 
+    password1 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':'Contraseña'
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        label='Contraseña',
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder':'Repetir Contraseña'
+            }
+        )
+    )
+
     class Meta:
 
+
         model = Usuarios
-        fields = ("__all__")
+        fields = ('username', 'email', 'first_name','last_name')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 != password2 :
+            self.add_error('password2', 'Las contraseñas no son iguales' )
+    
+    
+         # TODO Validation
+    
+        return password2
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
-from django.views.generic import CreateView
+from django.views.generic.edit import FormView
 from .forms import UsuarioRegisterForm
+from .models import Usuarios
 
 # Create your views here.
 
 
-class UsuarioRegisterView(CreateView):
+class UsuarioRegisterView(FormView):
     template_name = "usuarios/register.html"
     form_class = UsuarioRegisterForm
     success_url = '/'
+
+    def form_valid(self, form):
+
+        Usuarios.objects.create_user(
+            form.cleaned_data['username'],
+            form.cleaned_data['email'],
+            form.cleaned_data['password1'],
+            first_name=form.cleaned_data['first_name'],
+            last_name=form.cleaned_data['last_name'],
+        )
+
+        return super(UsuarioRegisterView, self).form_valid(form)
